1b72851123aa90424cd33e729e5a411e48e8ebe9
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-12-15 13:41:54 +0700

    Logs API.

--- app/Http/Controllers/ApiNetworkRouterLogController.php
+++ app/Http/Controllers/ApiNetworkRouterLogController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Formatter;
 use App\NetworkRouter;
-use Illuminate\Database\Eloquent\Collection;
 use RouterOS\Client as RouterOSClient;
 use RouterOS\Query as RouterOSQuery;
 
@@ -11,15 +11,33 @@ class ApiNetworkRouterLogController extends Controller
 {
     public function index(NetworkRouter $router)
     {
+        $logs = [];
+
         try {
             $client = new RouterOSClient([
                 'host' => $router->ipv4_address,
                 'user' => $router->admin_username,
                 'pass' => $router->admin_password,
             ]);
+
+            $log_query_results = $client->query(
+                (new RouterOSQuery("/log/print"))
+            )->read();
+
+            foreach ($log_query_results as $log_query_result) {
+                $temp = [];
+
+                foreach ($log_query_result as $key => $value) {
+                    $temp[Formatter::cleanKey($key)] = $value;
+                }
+
+                $logs[] = $temp;
+            }
         }
         catch (\Exception $e) {
             return $e->getMessage();
         }
+
+        return $logs;
     }
 }
